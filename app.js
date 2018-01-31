01d01370347f3cfc0ac01525ffe1e15afcb6b0a5
Adam LeBlanc <leblanca37@gmail.com>
2018-01-30 20:36:56 -0400

    Update the weeks forecast

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
   "use strict";
   // You will need your own api key that you can get by making an account at openweathermap.org
   // It is generally bad to include it in your source code, but for this example it's fine
-  const API_KEY = "<Your api key here>";
+  const API_KEY = "<Your API KEY HERE>";
   // This is the route of the API. This is the website we will ask for JSON from
   // To get the weather
   const BASE_URL = "https://api.openweathermap.org/data/2.5";
@@ -28,13 +28,17 @@
   // this is the extra info the api will use to give us what we need. Query params are
   // separated with & signs, so we can have multiple
   const WEATHER_URL = `${BASE_URL}/weather?APPID=${API_KEY}`;
+  // This is the url for future weather
+  const FUTURE_URL = `${BASE_URL}/forecast?APPID=${API_KEY}`;
 
   // we make an app object to hold useful state, such as the selected city.
   // We will also put a few functions on app.
   const app = {
     selectedCity: "Charlottetown",
     card: document.querySelector(".cardTemplate"),
-    units: "metric"
+    units: "metric",
+    // daysOfWeek index line up with the number JS uses for days
+    daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
   };
 
   // This function will do work in the background, so we add async to it.
@@ -48,11 +52,48 @@
     return await results.json();
   };
 
-  app.updateWeather = async () => {
-    // Grab the current weather from the function we made earlier
-    app.updateCurrentWeather(await app.fetchCurrentWeather());
+  // This function will do work in the background so we add async to it.
+  // it will fetch the weather for the next week and return it as json
+  app.fetchFutureWeather = async () => {
+    // Same as fetchCurrentWeather, except different URL
+    const url = `${FUTURE_URL}&q=${app.selectedCity}&units=${app.units}`;
+    const results = await fetch(url);
+    return await results.json();
   };
 
+  // Update the entire weather
+  app.updateWeather = async () => {
+    // Grab the current weather from the function we made earlier
+    // and pass it to the UI update function
+    app.updateCurrentWeather(await app.fetchCurrentWeather());
+    app.updateFutureWeather(await app.fetchFutureWeather());
+  };
+
+  // Handle updating the weekly forecast
+  app.updateFutureWeather = bulk => {
+    // take the bulk weather data and turn it into something we can use
+    let weather = averageWeather(bulk);
+    // get each day div and store in array
+    const days = app.card.querySelectorAll(".oneday");
+    // for each element in the array of days, fill in the content
+    days.forEach((value, index) => {
+      // fill in the day of the week. Uses the array we made on app
+      // to get the right shorthand
+      value.querySelector(".date").textContent =
+        app.daysOfWeek[new Date(weather[index].dt).getDay()];
+      // set the icon for the day
+      value.querySelector(".icon").style.backgroundImage = `url(${app.getIcon(
+        weather[index].icon
+      )})`;
+      // set the high and low temps
+      value.querySelector(".temp-high > .value").textContent =
+        weather[index].max;
+      value.querySelector(".temp-low > .value").textContent =
+        weather[index].min;
+    });
+  };
+
+  // Update teh current weather AI
   app.updateCurrentWeather = weather => {
     // alias the card we need to fill in
     const card = app.card;
@@ -82,10 +123,10 @@
     } %`;
     current.querySelector(".description > .wind > .value").textContent =
       weather.wind.speed;
-    current.querySelector(".description > .wind > .direction").textContent = `${
-      weather.wind.deg
-    }`;
-    // again, we make the date with createDate, and pass that date to be formated by another function
+    current.querySelector(".description > .wind > .direction").textContent =
+      weather.wind.deg;
+
+    // again, we make the date with createDate, and pass that date to be formatted by another function
     current.querySelector(".sunrise").textContent = formatTime(
       createDate(weather.sys.sunrise)
     );
@@ -98,6 +139,82 @@
     // build the icon url with the icon code
     return `http://openweathermap.org/img/w/${code}.png`;
   };
+
+  // sorts the weather into buckets by dat
+  function bucketWeather(weather) {
+    // sort the weather by day and store it in f_weather (formatted_weather)
+    const f_weather = [...weather].sort(
+      (a, b) => createDate(a.dt) - createDate(b.dt)
+    );
+    let i = -1; // we be set to 0 on the first pass. Keeps track of bucket index
+    let last = null;
+    // go through each item and store teh results in the accumulator
+    const buckets = f_weather.reduce((accumulator, currentValue) => {
+      // if it's the next day, make a new bucket
+      if (last === null || last !== new Date(currentValue.dt_txt).getDay()) {
+        i++;
+        accumulator[i] = [];
+        // set this as the previous day
+        last = new Date(currentValue.dt_txt).getDay();
+      }
+      // drop the forecast into the bucket
+      accumulator[i].push(currentValue);
+      // hand back the buckets for the next loop
+      return accumulator;
+    }, []);
+    // return the bucketed weather
+    return buckets;
+  }
+
+  // reduce the buckets down to an average
+  function reduceWeather(days) {
+    // for every bucket (day) store the result in an array then return the array
+    return days.map(day => {
+      // start object
+      const initial = {
+        min: 0,
+        max: 0,
+        icon: {}
+      };
+      // reduce all the stats for a day down into one object
+      var sum = day.reduce((accumulator, currentValue) => {
+        // only keep the coldest and hottest daus
+        accumulator.min =
+          currentValue.main.temp_min < accumulator.min
+            ? currentValue.main.temp_min
+            : accumulator.min;
+        accumulator.max =
+          currentValue.main.temp_max > accumulator.max
+            ? currentValue.main.temp_max
+            : accumulator.max;
+        // keep track of how many times each icon shows up (we take the one that shows the most)
+        accumulator.icon[currentValue.weather[0].icon] = accumulator.icon[
+          currentValue.weather[0].icon
+        ]
+          ? accumulator.icon[currentValue.weather[0].icon] + 1
+          : 1;
+        accumulator.dt = currentValue.dt_txt;
+        return accumulator;
+      }, initial);
+      // found the temps
+      sum.min = Math.floor(sum.min);
+      sum.max = Math.floor(sum.max);
+      // reduce the icon dow to the one with the most
+      sum.icon = Object.keys(sum.icon).reduce(
+        (accumulator, currentValue) =>
+          !sum.icon[accumulator] ||
+          sum.icon[currentValue] > sum.icon[accumulator]
+            ? currentValue
+            : accumulator,
+        0
+      );
+      return sum;
+    });
+  }
+
+  function averageWeather(weather) {
+    return reduceWeather(bucketWeather(weather.list));
+  }
 
   function createDate(timeInSeconds) {
     // api gives time in seconds, we need it in mili
